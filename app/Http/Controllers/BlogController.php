8ec7ac4d3607f548c04f443d47e1e2f69bc14520
JulianRuano <julianrmajin@gmail.com>
2023-12-25 14:01:18 -0500

    Refactor BlogController methods for editing and updating blogs

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -48,7 +48,7 @@ class BlogController extends Controller
             'content' => 'required',
             'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
         ]);
- 
+
 
         $blog = new Blog();
         $blog -> user_id = 1;
@@ -97,24 +97,87 @@ class BlogController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Blog $blog)
+    public function edit($id)
     {
-        //
+        $blog = Blog::find($id);
+        //filtrar las imagenes del blog
+        $images = Image::where('blog_id', $id)->get();
+        $url = $images[0]->url;
+        //filtrar los contenidos del blog
+        $contents = Content::where('blog_id', $id)->get();
+
+        return view('blogs.edit', [
+            'blog' => $blog,
+            'categories' => Category::all(),
+            'images' => $images,
+            'contents' => $contents,
+        ]);      
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Blog $blog)
+    public function update(Request $request, $id)
     {
-        //
+        $request -> validate([
+            'title' => 'required',
+            'category_id' => 'required',
+            'description' => 'required|max:200',
+            'slug' => 'required',
+            'alt' => 'required',
+            'content' => 'required',
+            //en caso de no subir imagen signfica que no se actualiza
+            'image' => 'image|mimes:jpeg,png,jpg|max:2048|different:image',
+        ]);
+
+        $blog = Blog::find($id);
+        $blog -> user_id = 1;
+        $blog -> title = $request -> title;
+        $blog -> category_id = $request -> category_id;
+        $blog -> description = $request -> description;
+        $blog -> slug = $request -> slug;
+
+        $blog -> save();
+
+        //revisamos si es la misma imagen y no se actualiza
+        if($request->file('image') != null){
+            // guardar imagen en storage/app/public/images
+            $img = $request->file('image');
+            $image_name = time().'.'.$img->extension();
+
+            $img->storeAs('public/images', $image_name);
+
+            // guardar imagen en la base de datos
+            $image = new Image();
+            $image -> blog_id = $blog -> id;
+            $image -> url = $image_name;
+            $image -> alt = $request -> alt;
+            $image -> position = 1;
+
+            $image -> save();
+        }
+
+        $content = new Content();
+        $content -> blog_id = $blog -> id;
+        $content -> title = $request -> title;
+        $content -> position = 1;
+        $content -> text = $request -> content;
+
+        $content -> save();
+
+        return redirect() -> route('blogs.index')
+            -> with('success', 'Blog actualizado correctamente.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Blog $blog)
+    public function destroy($id)
     {
-        //
+        $blog = Blog::find($id);
+        $blog -> delete();
+
+        return redirect() -> route('blogs.index')
+            -> with('success', 'Blog eliminado correctamente.');
     }
 }
--- resources/views/blogs/edit.blade.php
+++ resources/views/blogs/edit.blade.php
@@ -4,16 +4,117 @@
 
 @section('content')
 <main class="flex justify-center items-center h-screen bg-gray-200">
-    <div class="w-1/3 bg-white rounded-lg shadow-lg p-4">
-        <h1 class="text-4xl font-bold text-center mb-8">Editar Usuario</h1>
-        <form action="/users/{{ $user->id }}" method="POST">
+    <div class="w-2/3 bg-white rounded-lg shadow-lg p-4">
+        <h1 class="text-4xl font-bold text-center mb-8">Editar Blog</h1>
+        @if ($errors->any())
+            <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                Errores
+            </div>
+            <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700 mb-4">
+                <p><strong>Por favor corrige los siguientes errores:</strong></p>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li class="list-disc ml-4">{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        <form action="/blogs/{{ $blog->id }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
+            <div class="mb-4">
+                <label for="title" class="sr-only">Titulo</label>
+                <input type="text" name="title" id="title" placeholder="Titulo" value="{{ $blog->title }}" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('title') border-red-500 @enderror">
+                @error('title')
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}               
+                    </div>
+                @enderror
+            </div>
+            <div class="mb-4">
+                <label for="description" class="sr-only">Descripción</label>
+                <textarea name="description" id="description" cols="30" rows="2" placeholder="Descripción" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('description') border-red-500 @enderror">{{ $blog->description }}</textarea>
+                @error('description')
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}               
+                    </div>
+                @enderror
+            </div>              
+            <div class="mb-4">
+                <label for="category_id" class="sr-only">Categoría</label>
+                <select name="category_id" id="category_id" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('category_id') border-red-500 @enderror">
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}" {{ $blog->category_id == $category->id ? 'selected' : '' }}>{{ $category->name }}</option>    
+                    @endforeach
+                </select>
+                @error('category_id')
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}               
+                    </div>
+                @enderror
+            </div>
+            <div class="mb-4">
+                <label for="slug" class="sr-only">Slug</label>
+                <input type="text" name="slug" id="slug" placeholder="Slug" value="{{ $blog->slug }}" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('slug') border-red-500 @enderror">
+                @error('slug')
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}               
+                    </div>
+                @enderror
+            </div>
+
+            <div class="mb-4">
+                <label for="content" class="sr-only">Contenido</label>
+                <textarea name="content" id="content" cols="30" rows="2" placeholder="Contenido" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('content') border-red-500 @enderror">{{ $contents[0]-> text}}</textarea>
+                @error('content')
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}               
+                    </div>
+                @enderror
+            </div>
+
+            <div class="mb-4">
+                <label for="image" class="sr-only">Imagen</label>
+                //pedir imagen pero cargar los datos que ya estan en caso que no suba imagen
+                <input type="file" name="image" id="image" accept="image/*" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('image') border-red-500 @enderror">
+                <img id="preview" class="mt-2" src="{{ asset('storage/images/'.$images[0]->url)}} " alt="{{ $images[0]->alt }}" width="300px">
+                @error('image')
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}               
+                    </div>
+                @enderror
+            </div>
+
+            <div class="mb-4">
+                <label for="alt" class="sr-only">Alt</label>
+                <input type="text" name="alt" id="alt" placeholder="Alt" value="{{ $images[0]->alt  }}" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('alt') border-red-500 @enderror">
+                @error('alt')
+                    <div class="text-red-500 mt-2 text-sm">
+                        {{ $message }}               
+                    </div>
+                @enderror
+            </div>
 
             <div>
-                <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Editar Usuario</button>
+                <a href="/blogs" class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+                    Cancelar
+                </a>
+                <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">
+                    Editar Blog
+                </button>
             </div>
         </form>
     </div>
+    <script>
+        const image = document.getElementById('image');
+        const preview = document.getElementById('preview');
+        image.addEventListener('change', function(e){
+            const reader = new FileReader();
+            reader.onload = function(){
+                preview.src = reader.result;
+            }
+            reader.readAsDataURL(e.target.files[0]);
+        });
+    </script>
 </main>
 @endsection
